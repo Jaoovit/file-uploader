59a27fe425619dcbbf59ba498f775f975de0cd9d
João Oliveira <jaoovit@gmail.com>
2024-09-09 18:30:15 +0100

    config the EJS enginne

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("node:path");
 const initializeSession = require("./config/session");
 
 const app = express();
 
+// Middleware to parte JSON and URL-encoded data
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// EJS
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Prisma session configuration
 initializeSession(app);
 
 const PORT = process.env.PORT || 5500;
