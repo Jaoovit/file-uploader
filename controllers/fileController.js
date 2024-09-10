69b9f5832cbf1410ce698a07660fa0cb6b404484
João Oliveira <jaoovit@gmail.com>
2024-09-10 20:47:09 +0100

    create a system to upload files in the database

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,3 +1,34 @@
 const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
+
+const uploadFile = async (req, res) => {
+  try {
+    const {
+      originalname: fileName,
+      size: fileSize,
+      mimetype: mimeType,
+      path: filePath,
+    } = req.file;
+
+    const userId = req.session.userInfo.id;
+    const uploadTime = new Date();
+
+    const newFile = await prisma.file.create({
+      data: {
+        name: fileName,
+        size: fileSize,
+        mimeType: mimeType,
+        path: filePath,
+        uploadTime: uploadTime,
+        userId: userId,
+      },
+    });
+    console.log(newFile);
+    res.render("homepage", { file: "file" });
+  } catch (error) {
+    res.status(500).send("Error registering file");
+  }
+};
+
+module.exports = { uploadFile };
--- routes/fileRouter.js
+++ routes/fileRouter.js
@@ -2,4 +2,10 @@ const express = require("express");
 const fileController = require("../controllers/fileController");
 const router = express.Router();
 
+// Multer middleware
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+router.post("/document", upload.single("document"), fileController.uploadFile);
+
 module.exports = router;
